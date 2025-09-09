d2bbcbd1e48b022143567d87cd2f1ecfef9fd8f3
NadezhdaDokina <dokina.nadya@bk.ru>
2025-09-09 13:07:41 +0300

    Add files via upload

---
+++ Nadia.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+
+//Создание списка на языке C++
+list <string> li {"'a','2','b','4','c','6'"};
+
+//Создание stack на языке C++
+stack <string> stack;
+stack.push("a");
+stack.push("b");
+stack.push("c");
