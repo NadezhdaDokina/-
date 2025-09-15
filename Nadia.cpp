5120025271faa8df2e7ea17a96dfe08d95897a8c
NadezhdaDokina <dokina.nadya@bk.ru>
2025-09-15 23:08:02 +0300

    Update Nadia.cpp

--- Nadia.cpp
+++ Nadia.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-#include <list>
-using namespace std;
 
-
-int main()
-{
-    list<int> listData;
-    
-    listData.push_back(5);
-    listData.push_back(15);
-    listData.push_back(25);
-    listData.push_back(35);
-
-    return 0;
