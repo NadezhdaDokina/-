449bb74e2ac8564ecc1058c0ba6e9eebee90d037
NadezhdaDokina <dokina.nadya@bk.ru>
2025-11-15 14:54:52 +0300

    Update лр1.java

--- ЛР1/лр1.java
+++ ЛР1/лр1.java
@@ -1,1 +1,9 @@
+//Создание списка на языке Java
+string[] li = new string[] {"n","2","d","4","s","6"};
 
+//Создание stack на языке Java
+MyStack stack = new MyStack();
+stack.push("q");
+stack.push("w");
+
+stack.push("e");
