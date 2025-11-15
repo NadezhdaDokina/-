25b9bd555f4cb518f9c0be2afbdf98fba5d75503
NadezhdaDokina <dokina.nadya@bk.ru>
2025-11-15 14:55:10 +0300

    Update лр1.py

--- ЛР1/лр1.py
+++ ЛР1/лр1.py
@@ -1,1 +1,10 @@
+#Создание массива данных типа 'список' на python
+pi2pi = ['REY', 'dina', 'PLEX']
+pi2.append('EMIN')
 
+#Органзация стека на python
+stack = []
+
+stack.append('Nadia')
+stack.append('Ali')
+stack.append('Yli')
